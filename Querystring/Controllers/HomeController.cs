5282c14e60d306f50bbca8ecdd1e1caa7296076d
yamikjg4 <yamikgandhi@outlook.com>
2022-02-20 16:21:10 +0530

    new

---
+++ Querystring/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Querystring.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Querystring.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult querystring(string firstname ,string lastname)
+        {
+            ViewBag.fname = firstname;
+            ViewBag.lname = lastname;
+            return View();
+        }
+    }
+}
